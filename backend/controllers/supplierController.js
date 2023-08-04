12a8eadba2a6872c7080c8c4d3f8dd43df2db0d2
hamdi-khsib <khsib.hamdi@esprit.tn>
2023-08-04 16:02:25 +0100

    Update auth controller and auth routes names and install new packages

--- backend/controllers/supplierController.js
+++ backend/controllers/supplierController.js
@@ -1,4 +1,6 @@
 import Supplier from "../models/Supplier";
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
 
 /* read */
 
@@ -11,3 +13,14 @@ export const getSupplier = async (req, res) => {
         res.status(404).json({ error: err.message})
     }
 }
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllSuppliers = asyncHandler (async (req,res) => {
+    const users = await User.find().select('-password').lean()
+    if (!users?.length) {
+        return res.status(400).json({ message: 'No users found'})
+    }
+    res.json(users)
+})

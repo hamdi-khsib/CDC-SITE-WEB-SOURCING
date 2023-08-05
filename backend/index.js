84c45ecf5d32b41bfef0255a9cb92801562e4d4b
hamdi-khsib <khsib.hamdi@esprit.tn>
2023-08-05 16:11:38 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,6 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 

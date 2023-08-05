15af509b47b46f72bf53056272b5d33bd6496fb2
hamdi-khsib <khsib.hamdi@esprit.tn>
2023-08-05 15:54:07 +0100

    Update supplierRoutes.js

--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import supplierController from "../controllers/supplierController.js"
-import { verifyToken } from "../middleware/auth.js"
+import { verifyJWT } from "../middleware/verifyJWT.js"
 
 const router = express.Router()
 
+router.use(verifyJWT)
 /* read */
-router.get("/", verifyToken, supplierController.getAllSuppliers)
+router.get("/", supplierController.getAllSuppliers)
 
 /* create */
 router.post("/", supplierController.createNewSupplier)
